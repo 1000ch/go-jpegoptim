7b8701fb24b751a0b0df5ddd03df18c6b2a92c68
1000ch <shogo.sensui@gmail.com>
2014-10-06 11:39:35 +0900

    add options

--- jpegoptim.go
+++ jpegoptim.go
@@ -1,10 +1,11 @@
 package main
 
 /*
-#cgo amd64 386 CFLAGS: -DX86=1
-#cgo LDFLAGS: -L/usr/local/Cellar/jpeg-turbo/1.3.1/lib
-#cgo CPPFLAGS: -I/usr/local/Cellar/jpeg-turbo/1.3.1/include
 #include <stdio.h>
+#cgo amd64 386 CFLAGS: -DX86=1
+#cgo LDFLAGS: -L/usr/local/Cellar/jpeg-turbo/1.3.1/lib -L/usr/local/Cellar/jpeg/8d/lib/
+#cgo CFLAGS: -I/usr/local/Cellar/jpeg-turbo/1.3.1/include -I/usr/local/Cellar/jpeg/8d/include
+#include <jpeglib.h>
 #include "jpegoptim-src/jpegoptim.h"
 extern void jpeg_memory_dest(j_compress_ptr cinfo, unsigned char **bufptr, size_t *bufsizeptr, size_t incsize);
 */
