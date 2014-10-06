9ac540b8c0b5a021c845487033759a7108e38adb
1000ch <shogo.sensui@gmail.com>
2014-10-06 10:54:30 +0900

    debug

--- jpegoptim.go
+++ jpegoptim.go
@@ -1,6 +1,9 @@
 package main
 
 /*
+#cgo amd64 386 CFLAGS: -DX86=1
+#cgo LDFLAGS: -L/usr/local/Cellar/jpeg-turbo/1.3.1/lib
+#cgo CPPFLAGS: -I/usr/local/Cellar/jpeg-turbo/1.3.1/include
 #include <stdio.h>
 #include "jpegoptim-src/jpegoptim.h"
 extern void jpeg_memory_dest(j_compress_ptr cinfo, unsigned char **bufptr, size_t *bufsizeptr, size_t incsize);
