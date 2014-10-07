55a9edc52125d5b3a0bcd9ba52810fbedcc906d0
1000ch <shogo.sensui@gmail.com>
2014-10-07 15:26:25 +0900

    update files

--- Makefile
+++ Makefile
@@ -1,13 +1,28 @@
+GO=go
 CGO=go tool cgo
+REPO=git@github.com:tjko/jpegoptim.git
 SRC=jpegoptim-src
 
 default: build
 
 $(SRC):
 	@mkdir $(SRC)
-	@git clone git@github.com:tjko/jpegoptim.git $(SRC)
+	@git clone $(REPO) $(SRC)
 
 build: $(SRC) jpegoptim.h jpegoptim.go
-	@$(CGO) -debug-gcc jpegoptim.go
+	# configure
+	@cd $(SRC);./configure --prefix=`pwd`/local
+
+	# apply patch
+	@patch -u $(SRC)/Makefile < Makefile.patch
+
+	# comment out main function in jpegoptim.c
+	@sed -e "287,854d" $(SRC)/jpegoptim.c
+
+	# make
+	@cd $(SRC);make all
+
+	# build
+	@$(GO) build jpegoptim.go
 
 .PHONY: clean build
--- jpegoptim.go
+++ jpegoptim.go
@@ -3,8 +3,8 @@ package main
 /*
 #include <stdio.h>
 #cgo amd64 386 CFLAGS: -DX86=1
-#cgo LDFLAGS: -L/usr/local/Cellar/jpeg-turbo/1.3.1/lib -L/usr/local/Cellar/jpeg/8d/lib/
-#cgo CFLAGS: -I/usr/local/Cellar/jpeg-turbo/1.3.1/include -I/usr/local/Cellar/jpeg/8d/include
+#cgo LDFLAGS: -L/usr/local/Cellar/jpeg/8d/lib/ -L./jpegoptim-src -ljpegoptim -ljpeg
+#cgo CFLAGS: -I/usr/local/Cellar/jpeg/8d/include
 #include <jpeglib.h>
 #include "jpegoptim-src/jpegoptim.h"
 extern void jpeg_memory_dest(j_compress_ptr cinfo, unsigned char **bufptr, size_t *bufsizeptr, size_t incsize);
@@ -13,6 +13,5 @@ import "C"
 import "fmt"
 
 func main() {
-	C.jpeg_memory_dest()
 	fmt.Println("jpegoptim.go main")
 }
